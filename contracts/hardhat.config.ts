828756c4e1c9edd28d1ec79b262a628fcb3ee910
Truong Nguyen <vonguyen.0407@gmail.com>
2025-10-29 08:57:09 +0700

    setup config and .env to deploy sepoliaETH

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,7 +1,15 @@
 import type { HardhatUserConfig } from "hardhat/config";
 
 import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
-import { configVariable } from "hardhat/config";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+// Kiểm tra biến môi trường và ném lỗi nếu chúng không được đặt
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const SEPOLIA_PRIVATE_KEY = process.env.PRIVATE_KEY;
+if (!SEPOLIA_RPC_URL) throw new Error("Missing SEPOLIA_RPC_URL in .env");
+if (!SEPOLIA_PRIVATE_KEY) throw new Error("Missing PRIVATE_KEY in .env");
 
 const config: HardhatUserConfig = {
   plugins: [hardhatToolboxViemPlugin],
@@ -33,8 +41,8 @@ const config: HardhatUserConfig = {
     sepolia: {
       type: "http",
       chainType: "l1",
-      url: configVariable("SEPOLIA_RPC_URL"),
-      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+      url: SEPOLIA_RPC_URL,
+      accounts: [SEPOLIA_PRIVATE_KEY],
     },
   },
 };
