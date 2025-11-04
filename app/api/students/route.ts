d844d6abe8017b69758571dc505b23d9f6cf659a
Sinoo <khoasinno@gmail.com>
2025-11-04 14:13:41 +0700

    update permission  route getAllStudent

--- app/api/students/route.ts
+++ app/api/students/route.ts
@@ -1,8 +1,35 @@
 import { UserService } from "@/core/services/user.service";
+import { auth } from "@/lib/auth";
+import { prisma } from "@/lib/db";
 import { NextResponse } from "next/server";
 
 const userService = new UserService();
 export async function GET() {
+  // Xác thực người dùng với NextAuth
+  const session = await auth();
+  if (!session?.user?.id) {
+    return NextResponse.json(
+      { error: "Unauthorized - Vui lòng đăng nhập." },
+      { status: 401 }
+    );
+  }
+
+  // Kiểm tra role - CHỈ ADMIN mới được tạo certificate
+  const currentUser = await prisma.user.findFirst({
+    where: { id: parseInt(session.user.id) },
+  });
+
+  if (!currentUser) {
+    return NextResponse.json({ error: "User không tồn tại." }, { status: 404 });
+  }
+
+  if (currentUser.role !== "ADMIN") {
+    return NextResponse.json(
+      { error: "Forbidden - Chỉ Admin mới có thể GET Student list." },
+      { status: 403 }
+    );
+  }
+
   const users = await userService.getAllStudents();
   return NextResponse.json(users);
 }
